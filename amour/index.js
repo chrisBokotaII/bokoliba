14eb8c5ef5253b85f5885030ac5513371b753b46
chrisBokotaII <105431097+chrisBokotaII@users.noreply.github.com>
2023-03-01 12:33:16 +0300

    Add files via upload
    first commit

---
+++ amour/index.js
@@ -0,0 +1,35 @@
+const images= document.querySelectorAll(".images")
+const pics=["image1","image2","image3","image4","image5","image6","image7","image8"]
+const q1= document.getElementById("Q1").value;
+const q2= document.getElementById("Q2").value;
+
+for (let i = 0; i < images.length; i++) {
+   const item= images[i]
+ item.onmouseover=function () {
+for (let i = 0;i< pics.length;i++){
+  const randomNum= Math.floor(Math.random()*7+1)
+   const element = pics[randomNum];
+   document.body.style.backgroundImage = `url(./images/${element}.jpg)`
+}  
+
+      
+   }
+   
+}
+
+function question(){
+  
+   if(q1&&q2 ==="yes"||"oui"){
+       alert("are you sure?")
+   }else{
+     alert("why?????????????")
+   }
+}
+
+function hello(){
+   alert("hello")
+}
+
+set(hello,1000);
+
+ 
